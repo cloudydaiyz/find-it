6c5d2707366c325b5ce047b4b2a14fd7eb63d3f2
cloudydaiyz <kyland03.biz@gmail.com>
2024-08-05 09:38:00 -0500

    started lambda

--- functions/src/game.ts
+++ functions/src/game.ts
@@ -1,13 +1,26 @@
-import { viewPlayer } from "@cloudydaiyz/game-engine-lib";
+import { } from "@cloudydaiyz/game-engine-lib";
 import { Context, LambdaFunctionURLHandler } from "aws-lambda";
 import { ObjectId } from "mongodb";
+import assert from "assert";
 
 export const handler: LambdaFunctionURLHandler = async(event) => {
-    event.headers = {};
-    
-    viewPlayer(new ObjectId(""), "");
-    return {
+    const tokens = event.rawPath.split("/");
+    let resBody: string;
 
+    try {
+
+        assert(event.headers.token != undefined, "Must have a token for this operation");
+        resBody = "hi";
+    } catch(e) {
+        return {
+            statusCode: 400,
+            message: (e as Error).message
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: resBody
     };
 }
 
